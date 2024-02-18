624f1a4ca2d08e6db403b9436a0f6c7c35f34a79
josue <josuejero@hotmail.com>
2024-02-18 09:52:17 -0500

    navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
-export default function Navbar(){
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = () =>{
     return(
-        <main>
-            Navbar
-        </main>
+        <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <Image src="/logo.png" alt="Doctor Logo" width={300} height={100}/>
+            </div>
+        </nav>
     )
 }
+
+export default Navbar
