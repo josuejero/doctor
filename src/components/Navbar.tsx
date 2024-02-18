e9ac5d587edb43da4b77a938dba3ab316f80a5b7
Josue Jeronimo <josuejero@hotmail.com>
2024-02-18 13:52:54 -0500

    texting the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,52 @@ const Navbar = () =>{
         <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <Image src="/logo.png" alt="Doctor Logo" width={300} height={100}/>
-                <span className="font-semibold text-x1 tracking-tight">
-                    Doctor&apos;s Practice
-                </span>
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
-                    <Link href="/">
+                    <Link href="/" legacyBehavior>
                         <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
                             Home
                         </a>
                     </Link>
+                    <Link href="/who-we-are" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Who We Are
+                        </a>
+                    </Link>
+                    <Link href="/" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Our Services
+                        </a>
+                    </Link>
+                    <Link href="/" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Schedule Appointment
+                        </a>
+                    </Link>
+                    <Link href="/" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Volunteer
+                        </a>
+                    </Link>
+                    <Link href="/" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Our Team
+                        </a>
+                    </Link>
+                    <Link href="/" legacyBehavior>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Contact Us
+                        </a>
+                    </Link>
+                </div>
+                <div>
+                    <button className="inline-block text-sm px-4 py-2 leading none border rounded text-white border-white hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">
+                        Donate
+                    </button>
+                    <a href="mailto:josuejero@hotmail.com" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4">
+                        Email Us
+                    </a>
                 </div>
             </div>
         </nav>
---
+++ src/pages/who-we-are.tsx
@@ -0,0 +1,7 @@
+export default function WhoWeAre() {
+    return(
+        <div>
+            Who We Are
+        </div>
+    )
+}
