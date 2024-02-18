ced032e9c2ed3456d107058099f87983f7fc0df2
josue <josuejero@hotmail.com>
2024-02-18 13:33:54 -0500

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,18 @@ const Navbar = () =>{
         <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <Image src="/logo.png" alt="Doctor Logo" width={300} height={100}/>
+                <span className="font-semibold text-x1 tracking-tight">
+                    Doctor&apos;s Practice
+                </span>
+            </div>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="text-sm lg:flex-grow">
+                    <Link href="/">
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+                            Home
+                        </a>
+                    </Link>
+                </div>
             </div>
         </nav>
     )
